89991dff2a27e56afe13367da43022f8f1ca4e74
Steve Pritko <steven_pritko@us.ibm.com>
2023-10-17 09:26:54 -0400

    update version

--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ var GitCommit string
 // VersionPrerelease is the marker for version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var VersionPrerelease = "sdp"
+var VersionPrerelease = "sdp.2"
 
 // SemVersion is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
